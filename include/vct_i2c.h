6cc1171dab8302f56dd8832dedadde3a83927f11
pedrosc97 <pedro.sc.97@gmail.com>
2020-03-10 01:08:42 +0000

    Initial commit with code structure

---
+++ include/vct_i2c.h
@@ -0,0 +1,18 @@
+#ifndef VCT_I2C_H
+#define VCT_I2C_H
+
+#include <cstddef>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <cstdlib>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+
+typedef struct 
+
+}
+
+#endif
